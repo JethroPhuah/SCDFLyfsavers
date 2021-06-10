9808cbfd052f92dff23b6d40dadae20155b3f8ad
GoldenCorgi <42860714+GoldenCorgi@users.noreply.github.com>
2021-06-10 21:31:29 +0800

    v0.0.2

--- flutter_app/lib/Screens/functional/home_page.dart
+++ flutter_app/lib/Screens/functional/home_page.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 import 'line_chart_page.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar:
@@ -46,17 +50,66 @@ class HomePage extends StatelessWidget {
                       iconOn: Icons.done,
                       iconOff: Icons.warning_amber_rounded,
                       textSize: 18.0,
-                      onChanged: (bool position) {
-                        print("The button is $position");
+                      onChanged: (bool valveOn) async {
+                        //Turn ON valve
+                        if (valveOn) {
+                          //POST REQUEST
+                          print("POST REQUEST");
+                          var coordinates = "3.12345, 1.232123";
+                          var response = await openValve(coordinates, valveOn);
+                          if (response.statusCode == 200) {
+                            // If the server did return a 201 CREATED response,
+                            // then print msg
+                            print("The valve is $valveOn");
+                          } else {
+                            // If the server did not return a 201 CREATED response,
+                            // then throw an exception.
+                            throw Exception('Failed to turn $valveOn.');
+                          }
+                          //Turn OFF valve
+                        } else {
+                          //GET REQUEST
+                          print("GET REQUEST");
+                          // TODO: Turn off valve first, send valveOn == False to server
+                          // using openValve function like above.
+                          var pressure = await getPressure();
+                          print(pressure);
+                        }
                       },
                     ),
                     constraints:
                         BoxConstraints.tightForFinite(width: 200, height: 200),
                   ),
                 ],
-              )
+              ),
             ],
           ),
         ),
       );
+
+  Future<http.Response> openValve(String coordinates, bool valveOn) {
+    return http.post(
+      Uri.parse('https://scdf-lyfsavers.us-south.cf.appdomain.cloud/frontend/push_data'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, Map>{
+        'data': {'coordinates': coordinates, 'valveOn': valveOn}
+      }),
+    );
+  }
+
+  Future<Map> getPressure() async {
+    final response = await http.get(Uri.parse('https://scdf-lyfsavers.us-south.cf.appdomain.cloud/frontend/get_data'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return jsonDecode(response.body);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to retieve pressure');
+  }
+  }
 }
